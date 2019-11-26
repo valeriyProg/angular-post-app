da54a3c18bf4f9e4a58a92ecd9bf37f4f6cdd343
valeriyProg <val.fowork@gmail.com>
2019-11-26 21:55:44 +0200

    delete imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './dashboard.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-
-const routes: Routes = [ ];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
